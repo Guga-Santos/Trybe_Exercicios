bcbae421b4057951cf79cf6440b26b53c5b62eb2
GugaSantos <guga.santos.guga@gmail.com>
2021-11-26 18:37:49 -0300

    Criação da pasta 01 e do script01

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/01-Javascript-Primeiros_Passos/script01.js
@@ -0,0 +1,14 @@
+const myName = "Gustavo";
+const birthCity = "Brasília";
+let birthYear = 1994
+
+console.log(myName, birthCity, birthYear)
+
+birthYear = 2030 
+
+console.log(birthYear)
+
+birthCity = "Recife"
+
+console.log(birthCity)
+// Recebemos uma mensagem de erro porque uma constante não pode ser alterada.
