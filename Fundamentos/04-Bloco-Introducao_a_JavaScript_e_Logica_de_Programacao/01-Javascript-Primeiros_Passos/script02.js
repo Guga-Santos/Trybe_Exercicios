658b2b4a1713fdc40d059063200e04ec817a3f73
GugaSantos <guga.santos.guga@gmail.com>
2021-11-26 18:48:58 -0300

    Adicionado o exercicio 02

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/01-Javascript-Primeiros_Passos/script02.js
@@ -0,0 +1,23 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId, isEnrolled, patientInfo, patientEmail)
+
+console.log(typeof patientAge)
+
+// Exercício parte 02 :
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth
+
+console.log(area);
+
+const perimeter = (base + heigth) * 2
+
+console.log(perimeter)
