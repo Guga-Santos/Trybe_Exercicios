045e0e6b13ea90f7155015d99100a4fb32fb548b
GugaSantos <guga.santos.guga@gmail.com>
2021-11-27 00:53:33 -0300

    Adicionado exercicio-final 10

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/01-Javascript-Primeiros_Passos/script-exercicio-final-10.js
@@ -0,0 +1,11 @@
+function lucros(custo, venda) {
+    let imposto = (custo / 100) * 20
+let custoTotal = custo + imposto
+let lucro = venda - custoTotal
+
+console.log(lucro)
+}
+
+lucros(100,150)
+lucros(200,400)
+lucros(100,110)
