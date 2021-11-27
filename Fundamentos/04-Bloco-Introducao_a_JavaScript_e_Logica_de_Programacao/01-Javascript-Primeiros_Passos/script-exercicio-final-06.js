5cc2a35ac95f4426199fca6bb0294ae55c9e2984
GugaSantos <guga.santos.guga@gmail.com>
2021-11-26 23:53:17 -0300

    Adicionado exercício-final 06

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/01-Javascript-Primeiros_Passos/script-exercicio-final-06.js
@@ -0,0 +1,26 @@
+
+function xadrez(nome) {
+    let lower = nome.toLowerCase()
+
+    if(lower === "peão") {
+        console.log("Anda uma casa para frente")
+    } else if (lower === "cavalo") {
+        console.log("Anda três casas e uma casa em 90 graus")
+    } else if (lower === "bispo") {
+        console.log("Anda em diagonais")
+    } else if (lower === "torre") {
+        console.log("Anda para os lados, para frente e para trás")
+    } else if (lower === "rainha") {
+        console.log("Anda em qualquer direção")
+    } else if (lower === "rei") {
+        console.log("Anda em qualquer direção mas apenas uma casa")
+    } else {
+        console.log("ERROR - Esta peça não existe")
+    }
+}
+
+xadrez("BALEIA")
+xadrez("PEÃO")
+xadrez("peão")
+xadrez("Rei")
+xadrez("cavaLo")
