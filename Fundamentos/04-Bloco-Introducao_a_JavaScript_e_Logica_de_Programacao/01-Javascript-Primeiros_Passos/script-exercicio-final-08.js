dd77011b1f7295e2cba7ed43069ebdb2bab2a12b
GugaSantos <guga.santos.guga@gmail.com>
2021-11-27 00:24:34 -0300

    Adicionado exercicio-final 08

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/01-Javascript-Primeiros_Passos/script-exercicio-final-08.js
@@ -0,0 +1,15 @@
+function par(num1, num2, num3) {
+    if(num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+
+par(223,21,42)
+
+par(221,33,99)
+
+par(2,4,6)
+
+par(1,3,5)
