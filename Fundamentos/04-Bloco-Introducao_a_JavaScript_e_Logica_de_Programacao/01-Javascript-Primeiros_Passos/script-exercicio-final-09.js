d52116aa3359e160c3d8596c7ba2e8b9f37a6c51
GugaSantos <guga.santos.guga@gmail.com>
2021-11-27 00:27:23 -0300

    Adicionado exercicio-final 09

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/01-Javascript-Primeiros_Passos/script-exercicio-final-09.js
@@ -0,0 +1,15 @@
+function impar(num1, num2, num3) {
+    if(num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+
+impar(223,21,42)
+
+impar(221,33,99)
+
+impar(2,4,6)
+
+impar(1,3,5)
