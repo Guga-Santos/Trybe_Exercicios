9567a7dfeff5cc7e3de06879dc2bfcba600d7f9f
GugaSantos <guga.santos.guga@gmail.com>
2021-11-29 19:32:08 -0300

    Resolução dos exercícios-03

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-03.js
@@ -0,0 +1,11 @@
+/* Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos. */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var soma = 0
+for(let i = 0; i < numbers.length; i += 1) {
+    soma += numbers[i]
+}
+
+console.log(soma / (numbers.length))
