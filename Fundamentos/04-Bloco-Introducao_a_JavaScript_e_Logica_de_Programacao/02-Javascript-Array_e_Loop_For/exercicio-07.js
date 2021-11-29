0324628b1aa13d4e2516454e96b96730d8be2a1b
GugaSantos <guga.santos.guga@gmail.com>
2021-11-29 20:17:32 -0300

    Resolução do exercício 07

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-07.js
@@ -0,0 +1,11 @@
+// Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let menor = numbers[0]
+for(let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < menor) {
+        menor = i
+    }
+}
+console.log(menor)
