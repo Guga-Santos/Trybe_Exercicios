fd44b89a3be4f8aad6f271ac76db78f3efa49ec1
GugaSantos <guga.santos.guga@gmail.com>
2021-11-29 20:29:32 -0300

    Resolução do exercicio 08

--- Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-07.js
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-07.js
@@ -5,7 +5,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let menor = numbers[0]
 for(let i = 0; i < numbers.length; i += 1) {
     if (numbers[i] < menor) {
-        menor = i
+        menor = numbers[i]
     }
 }
 console.log(menor)
---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-08.js
@@ -0,0 +1,7 @@
+//  Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let array = [];
+for(let i = 0; i < 25; i += 1) {
+    array.push(i + 1)
+}
+console.log(array)
