b27876a2406f1233e540451ee5e34f08ccbd4d22
GugaSantos <guga.santos.guga@gmail.com>
2021-11-30 09:07:00 -0300

    Resolução do exercicio bonus 02

---
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-bonus-02.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 1; i < numbers.length; i += 1){
+   for(let j = 0; j < i; j += 1){
+    if(numbers[j] < numbers[i]) {
+      var auxiliar = numbers[j]
+        numbers[j] = numbers[i]
+        numbers[i] = auxiliar
+        
+    }
+   }
+}
+
+console.log(numbers)
