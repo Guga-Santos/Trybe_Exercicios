cbccdcf5b46fd4893add69fa12530edbeaadab96
GugaSantos <guga.santos.guga@gmail.com>
2021-11-30 00:46:47 -0300

    Resoluçao do primeiro exercicio bonus

--- Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-05.js
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-05.js
@@ -2,7 +2,7 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maior = 0
+let maior = numbers[0]
 for(let i = 0; i < numbers.length; i += 1) {
     let index = numbers[i]
     if (index > maior){
