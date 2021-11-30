a4ddc133680bd5f27fb5c8d343e2c95c7a9acfad
GugaSantos <guga.santos.guga@gmail.com>
2021-11-30 19:06:38 -0300

    Algumas alterações na pasta

--- Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-bonus-01.js
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/02-Javascript-Array_e_Loop_For/exercicio-bonus-01.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(let i = 1; i < numbers.length; i += 1){
-   for(let j = 0; j < i; j += 1){
+   for(let j = 0; j < numbers.length -1; j += 1){
     if(numbers[j] > numbers[i]) {
       var auxiliar = numbers[j]
         numbers[j] = numbers[i]
