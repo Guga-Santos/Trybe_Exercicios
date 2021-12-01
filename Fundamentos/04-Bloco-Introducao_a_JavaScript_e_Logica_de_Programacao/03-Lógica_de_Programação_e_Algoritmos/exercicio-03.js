c25d01f5528dda4e70644552460850dc357a492c
GugaSantos <guga.santos.guga@gmail.com>
2021-11-30 22:13:42 -0300

    Resolução do exercicio 03 - correcao

--- Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/03-Lógica_de_Programação_e_Algoritmos/exercicio-03.js
+++ Fundamentos/04-Bloco-Introducao_a_JavaScript_e_Logica_de_Programacao/03-Lógica_de_Programação_e_Algoritmos/exercicio-03.js
@@ -1,21 +1,5 @@
-/*function asteriscos(n) {
-    for(let i = n -2; i >= 0; i -= 1){
-        let linha = ''
-        for (let j = 0; j <= i; j += 1) {
-            linha += '.'
-        }
-        var asterisco = "*"
-        
-        console.log(linha + asterisco)
-    }
-}
-
-asteriscos(5) */
-
-
-
 function asteriscos(n) {
-    let posicao = n
+    let posicao = n //Aqui está a chave!
     for(let i = 0; i < n; i += 1){
         let linha = ''
         for(let j = 0; j <= n; j += 1){
