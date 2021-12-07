b978ccf641e49611f8fd1065a904d8d9206f4b5f
GugaSantos <guga.santos.guga@gmail.com>
2021-12-06 22:07:31 -0300

    Exercicios 1 do bloco, dia 2

---
+++ Fundamentos/05-Javascript-DOM_Eventos_WebStorage/02-Javascrips-Trabalhando_com_Elementos/script.js
@@ -0,0 +1,6 @@
+// arquivo script.js
+
+// console.log(document.getElementById('start').nextSibling) // nó
+
+// console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+
