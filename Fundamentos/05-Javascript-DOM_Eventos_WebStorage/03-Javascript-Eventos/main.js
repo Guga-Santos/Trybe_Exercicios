8b02637b75e463455f81c21087da963c15d440bc
GugaSantos <guga.santos.guga@gmail.com>
2021-12-08 19:27:32 -0300

    Finalizados os exercicios - Conheça o addEventListener

--- Fundamentos/05-Javascript-DOM_Eventos_WebStorage/03-Javascript-Eventos/main.js
+++ Fundamentos/05-Javascript-DOM_Eventos_WebStorage/03-Javascript-Eventos/main.js
@@ -35,8 +35,20 @@ input.addEventListener("input", function(event) {
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+myWebpage.addEventListener('dblclick', function (){
+    window.open('https://linkedin.com/in/gugasantos')
+})
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseenter', function(){
+    myWebpage.style.color = 'yellow'
+    myWebpage.style.cursor = 'pointer'
+    
+    myWebpage.addEventListener('mouseleave', function(){
+    myWebpage.style.color = 'white'
+})
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
