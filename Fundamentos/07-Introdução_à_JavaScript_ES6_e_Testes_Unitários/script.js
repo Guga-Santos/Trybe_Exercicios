6fb3c65d839cf68d7e5ab10ad7a17584c866c9aa
GugaSantos <guga.santos.guga@gmail.com>
2022-01-13 12:57:13 -0300

    Todos os exercicios feitos, exceto o ultimo

---
+++ Fundamentos/07-Introdução_à_JavaScript_ES6_e_Testes_Unitários/script.js
@@ -0,0 +1,17 @@
+const botao = document.querySelector('#btn');
+const h1 = document.querySelector('#contador');
+let contador = 0
+
+botao.addEventListener('click', () => {
+    contador += 1
+
+    // contador > 1 ? h1.innerHTML = `Você já clicou ${contador} vezes!` : h1.innerText = `Você já clicou ${contador} vez!`;
+
+    h1.innerHTML = `Você já clicou ${contador} ${contador > 1 ? 'vezes!' : 'vez!'}`;
+    
+    // if(contador == 1) {
+    //     h1.innerText = `Você já clicou ${contador} vez!`
+    // } else {
+    //     h1.innerText = `Você já clicou ${contador} vezes!`
+    // }
+})
