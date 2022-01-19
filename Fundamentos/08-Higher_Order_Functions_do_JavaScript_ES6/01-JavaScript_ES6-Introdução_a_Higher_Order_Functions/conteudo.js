0bc8e9a4f52b17e960c664a8e8d6e6638d503e7d
GugaSantos <guga.santos.guga@gmail.com>
2022-01-19 16:08:01 -0300

    conteudo 03

--- Fundamentos/08-Higher_Order_Functions_do_JavaScript_ES6/01-JavaScript_ES6-Introdução_a_Higher_Order_Functions/conteudo.js
+++ Fundamentos/08-Higher_Order_Functions_do_JavaScript_ES6/01-JavaScript_ES6-Introdução_a_Higher_Order_Functions/conteudo.js
@@ -10,12 +10,53 @@ printGreeting(sayHello);
 
 // ------------------------------
 
-const repeat = (number, action) => {
-    for (let count = 0; count <= number; count += 1) {
-      action(count);
-    }
-  };
+// const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
   
-  repeat(5, console.log);
+//   repeat(5, console.log);
 
 // ----------------------------------
+
+// const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
+  
+//   const isEven = (number) => {
+//       if (number % 2 === 0) {
+//           console.log(number, 'is even');
+//       }
+//   };
+
+//   const isOdd = (number) => {
+//       if((number % 2) > 0 ) {
+//           console.log(number, 'is odd');
+//       }
+//   };
+
+//   repeat(10, isEven);
+//   repeat(10, isOdd);
+
+// -------------------------------------------
+
+const alarm = () => {
+    return 'Acordando!!';
+};
+
+const comer = () => {
+    return 'Bora tomar café!!';
+};
+
+const dormir = () => {
+    return 'Partiu dormir!!';
+};
+
+const doingThings = (msg) => {
+    console.log(msg);
+};
+
+doingThings(dormir())
