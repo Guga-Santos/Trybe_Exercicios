f708df17c2ede7a760f3ed2afe2c26eb6b25e124
GugaSantos <guga.santos.guga@gmail.com>
2022-01-20 15:52:20 -0300

    Fixacao ok

---
+++ Fundamentos/08-Higher_Order_Functions_do_JavaScript_ES6/03-JavaScript_ES6-HigherOrderFunctions-map_e_filter/fixacao.js
@@ -0,0 +1,85 @@
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: '59' },
+        { name: 'Português', nota: '80' },
+        { name: 'Química', nota: '78' },
+        { name: 'Biologia', nota: '92' },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '76' },
+        { name: 'Português', nota: '90' },
+        { name: 'Química', nota: '70' },
+        { name: 'Biologia', nota: '80' },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '91' },
+        { name: 'Português', nota: '85' },
+        { name: 'Química', nota: '92' },
+        { name: 'Biologia', nota: '90' },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '70' },
+        { name: 'Português', nota: '70' },
+        { name: 'Química', nota: '60' },
+        { name: 'Biologia', nota: '50' },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '80' },
+        { name: 'Português', nota: '82' },
+        { name: 'Química', nota: '79' },
+        { name: 'Biologia', nota: '75' },
+      ],
+    },
+  ];
+
+  const reportStatus = (name, students) => {
+    const studentInfo = students.find((student) => student.nome === name);
+    return studentInfo.materias.map((materia) => (
+        `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+      ));
+  };
+  
+  console.log(reportStatus('Natalia', estudantes));
+
+//  Peça para retornar apenas o studentInfo que você entenderá!
