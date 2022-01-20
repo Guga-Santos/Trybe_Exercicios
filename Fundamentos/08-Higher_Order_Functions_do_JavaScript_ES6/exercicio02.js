cc80903f9384d29021e86b30677cf61708b1220f
GugaSantos <guga.santos.guga@gmail.com>
2022-01-20 03:19:33 -0300

    Exercicio 02 ok

---
+++ Fundamentos/08-Higher_Order_Functions_do_JavaScript_ES6/exercicio02.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Adicione o código do exercício aqui:
+
+//   Retorne o nome do livro de menor nome.
+//   Dica: use a função forEach .
+
+function smallerName() {
+    let nameBook= "Um titulo de livro extremamente longo para não ter erro."
+
+    books.forEach(element => {
+        if(element.name.length < nameBook.length) {
+            nameBook = element.name
+        }
+    });
+
+
+    return nameBook;
+  }
+
+  console.log(smallerName())
