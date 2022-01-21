2f69501f779ac791e5969910752f07a27c9e6a9d
GugaSantos <guga.santos.guga@gmail.com>
2022-01-21 11:36:16 -0300

    Ultimo exercicio aula feito

--- Fundamentos/08-Higher_Order_Functions_do_JavaScript_ES6/03-JavaScript_ES6-HigherOrderFunctions-map_e_filter/ExercicioAula03.js
+++ Fundamentos/08-Higher_Order_Functions_do_JavaScript_ES6/03-JavaScript_ES6-HigherOrderFunctions-map_e_filter/ExercicioAula03.js
@@ -22,8 +22,40 @@ const cities = [
     { state: 'CE', name: 'Jericoacoara', region: 'NE'},
 ];
 
+const regions = [
+    { short: 'N', name: 'Norte' },
+    { short: 'NE', name: 'Nordeste' },
+    { short: 'CW', name: 'Centroeste' },
+    { short: 'SE', name: 'Sudeste' },
+    { short: 'S', name: 'Sul' },
+];
+
 //  1 - Transforme (Mapeie) o array de objetos e retorne o seguite resultado:
 //  "A cidade de ${nomeDaCidade} fica no estado de ${nomeDoEstado}"
 const cityAndState = (array1, array2) => array1.map((objeto1) => `A cidade de ${objeto1.name} fica no estado de ${array2.find((objeto2) => objeto2.short === objeto1.state).name}`);
 
-console.log(cityAndState(cities, states));
+// console.log(cityAndState(cities, states));
+
+//  2 - Transforme (Mapeie) o array de objetos em um array de objetos com o seguinte formato:
+//  { state: "Amazonas", city: "Manaus", region: "Norte" }
+
+// const objectArray = cities.map((objeto1) => {
+//     const estados = states.find((objeto2) => objeto2.short === objeto1.state);
+//     const regiao = regions.find((objeto3) => objeto3.short === objeto1.region);
+//     return {
+//         state: estados.name,
+//         city: objeto1.name,
+//         region: regiao.name,
+//     }
+// });
+
+const objectArray = (array1, array2, array3) => array1.map((objeto1) => {
+    return {
+        state: `${array2.find((objeto2) => objeto2.short === objeto1.state).name}`,
+        city: objeto1.name,
+        region: `${array3.find((objeto3) => objeto3.short === objeto1.region).name}`,
+    }
+});
+
+console.log(objectArray(cities,states,regions))
+
