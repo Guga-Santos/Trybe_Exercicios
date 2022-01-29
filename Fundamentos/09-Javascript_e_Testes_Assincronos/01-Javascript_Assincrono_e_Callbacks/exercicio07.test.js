580d391417d3b22f0d19406970185dd56bd537bb
GugaSantos <guga.santos.guga@gmail.com>
2022-01-29 00:05:46 -0300

    Exercicio 07 ok - Que exercicio infernal, meu deus

---
+++ Fundamentos/09-Javascript_e_Testes_Assincronos/01-Javascript_Assincrono_e_Callbacks/exercicio07.test.js
@@ -0,0 +1,38 @@
+const { expect } = require("@jest/globals");
+const { doesNotMatch } = require("assert");
+const { default: expectationResultFactory } = require("jest-jasmine2/build/expectationResultFactory");
+const { testNameToKey } = require("jest-snapshot/build/utils");
+
+const uppercase = (str, callback) => {
+    setTimeout(() => {
+      callback(str.toUpperCase());
+    }, 500);
+  };
+
+  test('Testa se a função uppercase está funcionando', (done) => {
+    uppercase('gustavo', (str) => {
+        try {
+            expect(str).toBe('GUSTAVO');
+            done();
+        } catch(error) {
+            done(error);
+        }
+    });
+});
+
+// const uppercase = (str, callback) => {
+//     setTimeout(() => {
+//       callback(str.toUpperCase());
+//     }, 500);
+//   };
+  
+//   it('uppercase "test" to equal "TEST"', (done) => {
+//     uppercase('test', (str) => {
+//       try {
+//         expect(str).toBe('TEST');
+//         done();
+//       } catch (error) {
+//         done(error);
+//       }
+//     });
+//   });
