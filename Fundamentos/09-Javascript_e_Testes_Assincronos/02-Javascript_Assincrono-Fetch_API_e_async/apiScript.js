760b0ad93771853134834dfabe14d725c47e71dc
GugaSantos <guga.santos.guga@gmail.com>
2022-02-01 19:04:00 -0300

    Exercicio 01 ok

---
+++ Fundamentos/09-Javascript_e_Testes_Assincronos/02-Javascript_Assincrono-Fetch_API_e_async/apiScript.js
@@ -0,0 +1,16 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+    const myObject = {
+        method: 'GET',
+        headers: { 'Accept' : 'application/json'}
+    };
+
+    fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => document.querySelector('#jokeContainer').innerText = data.joke)
+};
+
+
+window.onload = () => fetchJoke()
