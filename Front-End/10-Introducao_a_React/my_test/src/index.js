321c2b28da6ca9f7ce7bb23aaeb17355c2352d5c
GugaSantos <guga.santos.guga@gmail.com>
2022-02-16 15:26:19 -0300

    Fixando 01

--- Front-End/10-Introducao_a_React/my_test/src/index.js
+++ Front-End/10-Introducao_a_React/my_test/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString('pt', { hour12: true })}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(tick, 1000);
