dbd974bbd79b5806cf07e10ec317e4268091a79c
GugaSantos <guga.santos.guga@gmail.com>
2022-02-16 16:13:07 -0300

    Fixacao Classes

---
+++ Front-End/10-Introducao_a_React/01-Introducao/fixacao/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Teste from './component';
+
+function App() {
+  return <Teste />
+}
+
+export default App;
---
+++ Front-End/10-Introducao_a_React/01-Introducao/fixacao/src/component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Gustavo Santos</h1>
+                <p>Aluno de desenvolvimento Web na Trybe!</p>
+            </div>
+        )
+    }
+}
+
+export default Teste
